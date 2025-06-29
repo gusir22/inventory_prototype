2823ebba85da299135fed16a919ecad1f9d0fe61
Gustavo Rivero <gusir22@gmail.com>
2025-06-29 16:28:54 -0400

    Created Inventory List Page

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views import IngredientListView
+
+
 urlpatterns = [
-    
+    path('inventory/', IngredientListView.as_view(), name="inventory"),
 ]
