fbf7ca602875bbba54d3dc16098a2c237f9f4b45
Gustavo Rivero <gusir22@gmail.com>
2025-07-01 12:58:20 -0400

    Created Menu View

--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import IngredientListView
+from .views import IngredientListView, MenuItemListView
 
 
 urlpatterns = [
     path('inventory/', IngredientListView.as_view(), name="inventory"),
+    path('menu/', MenuItemListView.as_view(), name="menu"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,16 @@
 from django.views.generic import ListView
 
-from .models import Ingredient
+from .models import Ingredient, MenuItem
 
 
 class IngredientListView(ListView):
     model = Ingredient
     template_name = "menu/inventory_list.html"
     context_object_name = "ingredients"
+
+
+class MenuItemListView(ListView):
+    model = MenuItem
+    template_name = "menu/menu_list.html"
+    context_object_name = "menu_items"
+    
