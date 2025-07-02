9b2dd645967ec0adde23b31fe19661e4ed7d8039
Gustavo Rivero <gusir22@gmail.com>
2025-07-02 00:38:51 -0400

    Styled Successful Order with Bootstrap5

--- orders/urls.py
+++ orders/urls.py
@@ -8,6 +8,6 @@ from .views import (
 
 urlpatterns = [
     path('place-order/', PlaceOrderView.as_view(), name="place_order"),
-    path('order-sucess/', OrderSuccessView.as_view(), name="order_success"),
+    path('order-success/', OrderSuccessView.as_view(), name="order_success"),
     path('sales-report/', SalesReportView.as_view(), name="sales_report"),
 ]
