dd66e934b24f16720cf6e694461e51fbbbee0e9d
Gustavo Rivero <gusir22@gmail.com>
2025-07-01 23:32:22 -0400

    Styled Place Order with Bootstrap5

--- orders/forms.py
+++ orders/forms.py
@@ -3,8 +3,21 @@ from django.forms import formset_factory
 from .models import MenuItem
 
 class CustomerOrderItemForm(forms.Form):
-    menu_item = forms.ModelChoiceField(queryset=MenuItem.objects.all())
-    quantity = forms.IntegerField(min_value=1, initial=1)
+    menu_item = forms.ModelChoiceField(
+        queryset=MenuItem.objects.all(),
+        widget=forms.Select(attrs={
+            'class': 'form-select text-center border-primary',
+            'aria-label': 'Select Menu Item'
+        })
+    )
+    quantity = forms.IntegerField(
+        min_value=1, 
+        initial=1,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control text-center border-primary mb-3',
+            'placeholder': 'Enter quantity'
+        })
+    )
 
 # You can increase `extra` to show more rows
 CustomerOrderFormSet = formset_factory(CustomerOrderItemForm, extra=5)
